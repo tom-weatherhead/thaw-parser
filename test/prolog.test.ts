6f168d5e8d99ce605839cf0f8d16579e22433f41
Tom Weatherhead <tomw@2hrd4u.org>
2021-06-18 15:47:33 -0600

    Dev

---
+++ test/prolog.test.ts
@@ -0,0 +1,104 @@
+// tom-weatherhead/thaw-parser/test/prolog.test.ts
+
+'use strict';
+
+import {
+	createTokenizer,
+	LexicalAnalyzerSelector
+} from 'thaw-lexical-analyzer';
+
+import {
+	createGrammar,
+	LanguageSelector,
+	PrologGlobalInfo
+} from 'thaw-grammar';
+
+import { createParser, ParserSelector } from '..';
+
+// **** Prolog ****
+
+test('LL(1) parser instance creation test - Prolog', () => {
+	// Arrange
+	const ls = LanguageSelector.Prolog2;
+	const grammar = createGrammar(ls);
+
+	// Act
+	const parser = createParser(ParserSelector.LL1, grammar);
+
+	// Assert
+	expect(parser).toBeTruthy();
+});
+
+function prologTest(
+	data: Array<[input: string, expectedResult: string]>
+): void {
+	// Arrange
+	const ls = LanguageSelector.Prolog2;
+	const prologGlobalInfo = new PrologGlobalInfo();
+	const grammar = createGrammar(ls);
+	const tokenizer = createTokenizer(LexicalAnalyzerSelector.MidnightHack, ls);
+	const parser = createParser(ParserSelector.LL1, grammar);
+
+	for (const [input, expectedResult] of data) {
+		// Act
+		const actualResult = prologGlobalInfo.ProcessInput(
+			parser.parse(tokenizer.tokenize(input))
+		);
+
+		// Assert
+		expect(actualResult).toBe(expectedResult);
+	}
+}
+
+// test('LL(1) Prolog interpret test 1', () => {
+// 	// Arrange
+// 	const ls = LanguageSelector.Prolog2;
+// 	const prologGlobalInfo = new PrologGlobalInfo();
+// 	const grammar = createGrammar(ls);
+// 	const tokenizer = createTokenizer(LexicalAnalyzerSelector.MidnightHack, ls);
+// 	const parser = createParser(ParserSelector.LL1, grammar);
+
+// 	const input1 = 'pred1.';
+// 	const input2 = '?- pred1.';
+// 	// const listOfTokens = tokenizer.tokenize(inputString);
+// 	// const parseResult = parser.parse(listOfTokens);
+
+// 	// console.log(
+// 	// 	`thaw-parser: Prolog test: parseResult of '${inputString}' is:`,
+// 	// 	typeof parseResult,
+// 	// 	parseResult
+// 	// );
+
+// 	// Act
+// 	// Assert
+// 	expect(
+// 		prologGlobalInfo.ProcessInput(parser.parse(tokenizer.tokenize(input1)))
+// 	).toBe(PrologGlobalInfo.ClauseAdded);
+// 	expect(
+// 		prologGlobalInfo.ProcessInput(parser.parse(tokenizer.tokenize(input2)))
+// 	).toBe(PrologGlobalInfo.Satisfied);
+// });
+
+test('LL(1) Prolog interpret test 1', () => {
+	prologTest([
+		// ['', PrologGlobalInfo.],
+		['pred1.', PrologGlobalInfo.ClauseAdded],
+		['?- pred1.', 'Satisfying substitution is: []\n' + PrologGlobalInfo.Satisfied]
+	]);
+});
+
+test('LL(1) Prolog interpret test 2', () => {
+	prologTest([
+		['foo :- bar, baz.', PrologGlobalInfo.ClauseAdded],
+		['bar.', PrologGlobalInfo.ClauseAdded],
+		['baz.', PrologGlobalInfo.ClauseAdded],
+		['?- foo.', 'Satisfying substitution is: []\n' + PrologGlobalInfo.Satisfied]
+	]);
+});
+
+test('LL(1) Prolog interpret test 3', () => {
+	prologTest([
+		['pred(V).', PrologGlobalInfo.ClauseAdded],
+		['?- pred(1337).', 'Satisfying substitution is: [?V -> 1337]\n' + PrologGlobalInfo.Satisfied]
+	]);
+});
